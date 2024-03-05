463ee415100fc499eb1cb801f49c0420c8d359b3
Aziz Maulana Hakim <145968654+aziz080904@users.noreply.github.com>
2024-03-05 12:46:17 +0700

    Add files via upload

---
+++ Pekan 1/variable_user.php
@@ -0,0 +1,11 @@
+<?php
+// variable user
+$nama = 'Aziz Maulana';
+$umur = 19;
+$berat = 70;
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+
+echo "<br/>Hello $nama Apa kabar?";
+?>
